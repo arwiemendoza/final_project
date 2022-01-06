88e99ef903894ca34bda54c6499ce08213166199
Arwie Mendoza <arwie@avionschool.com>
2022-01-06 23:18:18 +0800

    Added User Spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root 'home#index'
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,26 @@ RSpec.describe User, type: :model do
     let!(:user) {User.new}
 
     context 'Validations' do
+        it '1. Is not valid without first name' do
+            user.first_name = nil
 
+        expect(user).to_not be_valid
+        expect(user.errors).to be_present
+        end
+
+        it '2. Is not valid without last name' do
+            user.last_name = nil
+
+        expect(user).to_not be_valid
+        expect(user.errors).to be_present
+        end
+
+        it '1. User status is Pending by default' do
+            user.user_status = "Approved"
+
+        expect(user).to_not be_valid
+        expect(user.errors).to be_present
+        end
     end
 
 end
