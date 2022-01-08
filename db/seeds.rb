838f9b51719fde4538f8366e45af0f2e1e167568
Rpmedestomas <rpmedestomas@gmail.com>
2022-01-08 11:25:34 +0800

    added seed users, and header

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 admin = User.new(first_name: "admin", last_name: "admin", email:"admin@gmail.com", password:"123456", password_confirmation:"123456", admin:true, user_status: "Approved")
 admin.save
 
-helper = User.new(first_name: "admin", last_name: "admin", email:"helper@gmail.com", password:"123456", password_confirmation:"123456", helper:true, user_status: "Approved")
+helper = User.new(first_name: "helper", last_name: "helper", email:"helper@gmail.com", password:"123456", password_confirmation:"123456", helper:true, user_status: "Approved")
 helper.save
 
-user = User.new(first_name: "admin", last_name: "admin", email:"user@gmail.com", password:"123456", password_confirmation:"123456", user_status: "Approved")
+user = User.new(first_name: "user", last_name: "user", email:"user@gmail.com", password:"123456", password_confirmation:"123456", user_status: "Approved")
 user.save
