ce4fcac8698928fa95bae3757529083fa1f4859d
Starboy9000 <84158973+Starboy9000@users.noreply.github.com>
2022-01-10 10:39:44 +0800

    added 4 more tests to User_spec.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 admin = User.new(first_name: "admin", last_name: "admin", email:"admin@gmail.com", password:"123456", password_confirmation:"123456", admin:true, user_status: "Approved")
+admin.skip_confirmation!
 admin.save
 
 helper = User.new(first_name: "helper", last_name: "helper", email:"helper@gmail.com", password:"123456", password_confirmation:"123456", helper:true, user_status: "Approved")
+helper.skip_confirmation!
 helper.save
 
 user = User.new(first_name: "user", last_name: "user", email:"user@gmail.com", password:"123456", password_confirmation:"123456", user_status: "Approved")
+user.skip_confirmation!
 user.save
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,27 +26,51 @@ RSpec.describe User, type: :model do
         expect(user.errors).to be_present
         end
 
-        it '4. User status is Pending by default' do
+        it '4. Last name should have minimum of 3 characters' do
+            user.last_name = 'ab'
+
+        expect(user).to_not be_valid
+        expect(user.errors).to be_present
+        end
+
+        it '5. User status is Pending by default' do
             user.user_status = "Approved"
 
         expect(user).to_not be_valid
         expect(user.errors).to be_present
         end
 
-        it '5. User password should have minimum of 6 characters' do
+        it '6. User password should have minimum of 6 characters' do
             user.password = '12345'
 
         expect(user).to_not be_valid
         expect(user.errors).to be_present
         end
 
-        it '6. User password and confirm password should match' do
+        it '7. User password and confirm password should match' do
             user.password = '12345'
             user.password_confirmation = '12345667'
 
         expect(user).to_not be_valid
         expect(user.errors).to be_present
         end
+        
+        it '8. First name does not have special characters' do
+            user.first_name = "!@#$%^&*"
+
+        expect(user).to_not be_valid
+        expect(user.errors).to be_present
+        end
+
+        it '9. Last name does not have special characters' do
+            user.last_name = "!@#$%^&*"
+
+        expect(user).to_not be_valid
+        expect(user.errors).to be_present
+        end
+       
+        
+    
     end
 
 end
