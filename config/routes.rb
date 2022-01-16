de9bcdb85e18ae6c81370aae10016fc0d97cbfc1
Rolan Medestomas <rpmedestomas@gmail.com>
2022-01-16 09:46:20 +0800

    Fixed stuff

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
 
   get 'user_list' => 'admin#user_list', as: :user_list
 
-  patch '/:category_id/tasks/:id/accept_task' => 'tasks#accept_task', as: :accept_task
+  patch 'category/:category_id/tasks/:id/accept_task' => 'tasks#accept_task', as: :accept_task
 
 end
