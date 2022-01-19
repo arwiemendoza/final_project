39921d6ab77d5fefeeb7497f626c97f56421f493
Miyu <togomiyu@gmail.com>
2022-01-19 12:25:23 +0800

    removed format in task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :category
 
-    validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "can only include letters"}
+    validates :name, presence: true
     validates :details, presence: true
     validates :date, presence: true
     validates :hourly_rate, presence: true
