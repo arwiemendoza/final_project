d72aebcced27e1ab349ef0285c5dfefd3299dc47
Arwie Mendoza <arwie@avionschool.com>
2022-01-28 22:45:01 +0800

    Edited application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-require("packs/map_js")
-require("packs/current_location")
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
+require('packs/task_map_show');
+require('packs/task_map_input');
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
-
-
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
